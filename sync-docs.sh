349fcd9ae58d939df7d8d80c397fb785de454ea7
Nathaniel Butterworth <natbutter@gmail.com>
2026-02-25 04:53:06 +0000

    fixed broken rendering

--- sync-docs.sh
+++ sync-docs.sh
@@ -7,6 +7,6 @@ else
     BASE=".."
 fi
 
-tail -n +5 $BASE/prism-setup/README.md | sed 's/^#/##/' > guide/_setup_readme.qmd
-tail -n +8 $BASE/prism-flutter-backend/README.md | sed 's/^#/##/' > guide/_flutter-backend_readme.qmd
-tail -n +2 $BASE/prism-flutter-web-frontend/README.md | sed 's/^#/##/' > guide/_flutter-web-frontend_readme.qmd
+tail -n +5 $BASE/prism-setup/README.md | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_setup_readme.qmd
+tail -n +8 $BASE/prism-flutter-backend/README.md | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_flutter-backend_readme.qmd
+tail -n +2 $BASE/prism-flutter-web-frontend/README.md | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_flutter-web-frontend_readme.qmd
