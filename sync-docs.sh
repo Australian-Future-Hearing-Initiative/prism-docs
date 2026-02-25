9c8af65db24e9346e3c6fa95ed3c1343e95d45fe
Nathaniel Butterworth <natbutter@gmail.com>
2026-02-25 03:59:46 +0000

    fix?

--- sync-docs.sh
+++ sync-docs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-tail -n +5 ../prism-setup/README.md | sed 's/^#/##/' > guide/_setup_readme.qmd
-tail -n +8 ../prism-flutter-backend/README.md | sed 's/^#/##/' > guide/_flutter-backend_readme.qmd
-tail -n +2 ../prism-flutter-web-frontend/README.md | sed 's/^#/##/' > guide/_flutter-web-frontend_readme.qmd
+# Check if .deps exists (GitHub Actions mode), otherwise look at sibling folders (Local mode)
+if [ -d ".deps" ]; then
+    BASE=".deps"
+else
+    BASE=".."
+fi
+
+tail -n +5 $BASE/prism-setup/README.md | sed 's/^#/##/' > guide/_setup_readme.qmd
+tail -n +8 $BASE/prism-flutter-backend/README.md | sed 's/^#/##/' > guide/_flutter-backend_readme.qmd
+tail -n +2 $BASE/prism-flutter-web-frontend/README.md | sed 's/^#/##/' > guide/_flutter-web-frontend_readme.qmd
