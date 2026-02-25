82a8936212c7f436939625d9fa5f1ef6901c3363
Nathaniel Butterworth <natbutter@gmail.com>
2026-02-25 22:30:37 +0000

    access token approved

--- sync-docs.sh
+++ sync-docs.sh
@@ -16,6 +16,7 @@ for src in "$BASE/prism-setup/README.md" "$BASE/prism-flutter-backend/README.md"
     fi
 done
 
+# Many of the exisiting REAMDEs have slightly different formats, clean them here.
 tail -n +5 "$BASE/prism-setup/README.md" | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_setup_readme.qmd
 tail -n +8 "$BASE/prism-flutter-backend/README.md" | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_flutter-backend_readme.qmd
 tail -n +2 "$BASE/prism-flutter-web-frontend/README.md" | sed 's/^#/##/' | sed 's/^---$/<hr>/' > guide/_flutter-web-frontend_readme.qmd
